b6c8a27c7411e60ac1887b9594aad7f85c012082
ab4421 <ab4421@ic.ac.uk>
2025-05-08 12:57:41 +0100

    Initial Commit

---
+++ Masters_Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Masters_Project
+//
+//  Created by Arnav Bhatia on 08/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Masters_Project/Masters_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Masters_ProjectApp.swift
+//  Masters_Project
+//
+//  Created by Arnav Bhatia on 08/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Masters_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
